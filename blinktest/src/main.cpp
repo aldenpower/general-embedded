b53e72f91ea02fc01df4c634a792c1e588401cc1
aldenpower <anderson_afsl@hotmail.com>
2021-12-04 15:45:06 +0000

    add print and println serial example

--- blinktest/src/main.cpp
+++ blinktest/src/main.cpp
@@ -1,36 +1,48 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
+/*
+  Uses a for loop to print numbers in various formats.
+*/
+
 #include "Arduino.h"
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 13
-#endif
 
-int analogValue = 0;
-int setdelay = 1000;
-
-void setup()
-{
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
+void setup() {
+  Serial.begin(9600); // open the serial port at 9600 bps:
 }
 
-void loop()
-{
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
+void loop() {
+  // print labels
+  Serial.print("NO FORMAT");  // prints a label
+  Serial.print("\t");         // prints a tab
 
-  // wait for a second
-  delay(setdelay);
+  Serial.print("DEC");
+  Serial.print("\t");
 
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
+  Serial.print("HEX");
+  Serial.print("\t");
 
-  // wait for a second
-  delay(setdelay);
+  Serial.print("OCT");
+  Serial.print("\t");
+
+  Serial.print("BIN");
+  Serial.println();        // carriage return after the last label
+
+  for (int x = 0; x < 64; x++) { // only part of the ASCII chart, change to suit
+    // print it out in many formats:
+    Serial.print(x);       // print as an ASCII-encoded decimal - same as "DEC"
+    Serial.print("\t\t");  // prints two tabs to accomodate the label lenght
+
+    Serial.print(x, DEC);  // print as an ASCII-encoded decimal
+    Serial.print("\t");    // prints a tab
+
+    Serial.print(x, HEX);  // print as an ASCII-encoded hexadecimal
+    Serial.print("\t");    // prints a tab
+
+    Serial.print(x, OCT);  // print as an ASCII-encoded octal
+    Serial.print("\t");    // prints a tab
+
+    Serial.println(x, BIN);  // print as an ASCII-encoded binary
+    // then adds the carriage return with "println"
+    delay(200);            // delay 200 milliseconds
+  }
+  Serial.println();        // prints another carriage return
 }
