f1e5f08af3e1198b1c32b804c64f9e07ee7d754c
aldenpower <anderson_afsl@hotmail.com>
2022-01-05 12:03:06 -0300

    add print receive message arduino example

--- bridgebasic/src/main.cpp
+++ bridgebasic/src/main.cpp
@@ -2,13 +2,42 @@
 
 // Range of the integer (-32768 to 32767)
 // Size o the integer is 16-bit 2 bytes
-int x = 12;
+
+const unsigned int MAX_MESSAGE_LENGHT = 12;
 
 void setup(){
     Serial.begin(9600);
 }
  
 void loop(){
-   Serial.print(x);
-   delay(1000); 
+    // check to see if anything is available in the serial receive buffer
+    while (Serial.available() > 0) //available return the number of bytes to be read
+    {
+        // create a place to hold the incoming message
+        static char message[MAX_MESSAGE_LENGHT];
+        static unsigned int message_pos = 0;
+
+        // read the next available byte in the serial receiver buffer
+        char intByte = Serial.read();
+
+        // message coming check no terminating character new line
+        if (intByte != '\n' && (message_pos < MAX_MESSAGE_LENGHT - 1))
+        {
+            // add the coming byte to the message
+            message[message_pos] = intByte;
+            message_pos++;   
+        }
+        else
+        {
+            // add null character to string
+            message_pos[message_pos] = '\0';
+            Serial.println(message);
+
+            // reset for the next message
+            message_pos = 0;
+        }
+        
+    }
+    
+    
 }
