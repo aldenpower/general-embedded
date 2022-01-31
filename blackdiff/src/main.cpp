94adffba54fafce24dbc8ce91a3fda34eaab67c0
aldenpower <anderson_afsl@hotmail.com>
2022-01-31 08:18:21 -0300

    add first sonar test

--- blackdiff/src/main.cpp
+++ blackdiff/src/main.cpp
@@ -1,4 +1,4 @@
-// #include <HCSR04.h> // substituir por newping
+#include <NewPing.h>
 #include <ros.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Float64.h>
@@ -14,33 +14,36 @@
 //LEFT MOTOR
 #define enb_pin 6 
 #define in3_pin 4 
-#define in4_pin 3 
-//------------------------ DECLARE FUNCTIONS ----------------------------------------------
+#define in4_pin 3
+// SONAR SENSOR
+#define trigger = 2
+#define echo = 7
+//Declare functions
 void MotorTest(int percent1, int percent2, int pwmpin1, int pwmpin2);
 void motors_init();
 void messageCb( const geometry_msgs::Twist& msg);
-//------------------------ VARIABLES ------------------------------------------------------
-//MAX BLACKDIFF VEL IN m/s
+//Variables and objects
 double max_speed = 2;
 double left_speed = 0, right_speed = 0;
 double left_speed_percent = 0, right_speed_percent = 0;
 //RADIUS AND SEPARATION WHEELS
 double wheel_rad = 0.034, wheel_sep = 0.156; 
 double speed_ang = 0, speed_lin = 0;
-const long PERIODO = 500;
-unsigned long tempoAnterior = 0;
-//------------------------ ROS VARIABLES ------------------------------------------------------
+int distance = 0;
+int max_distance = 500;
+NewPing Sonar(trigger, echo, max_distance)
+//ROS objects
 ros::NodeHandle nh;
-// std_msgs::Float64 distancia;
-std_msgs::Int64 left; // PODE SER INTEIRO
+std_msgs::Float64 distance;
+std_msgs::Int64 left;
 std_msgs::Int64 right;
-//------------------------ PUBLISHERS ------------------------------------------------------
-// ros::Publisher pub_dist("distancia", &distancia);
+//Publisher
+Publisher pub_dist("distance", &distance);
 ros::Publisher pub_left("left", &left);
 ros::Publisher pub_right("right", &right);
-//------------------------ SUBSCRIBER ------------------------------------------------------
+//SUbscribers
 ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", &messageCb );
-//------------------------ CALLBACK ------------------------------------------------------
+//Callback
 void messageCb( const geometry_msgs::Twist& msg){
   speed_lin = msg.linear.x;
   speed_ang = msg.angular.z;
@@ -48,27 +51,32 @@ void messageCb( const geometry_msgs::Twist& msg){
   left_speed = speed_lin - (speed_ang * wheel_sep / 2);
   right_speed = speed_lin + (speed_ang * wheel_sep / 2);
 
-  left_speed_percent = 100 * left_speed / max_speed;
-  right_speed_percent = 100 * right_speed / max_speed;
+  left_speed_percent = left_speed / max_speed;
+  right_speed_percent = right_speed / max_speed;
 }
 
 void setup () {
-    // Serial.begin(9600);
+    Serial.begin(9600);
+
     motors_init();
     nh.initNode();
-    // nh.advertise(pub_dist);
+    nh.advertise(pub_dist);
     nh.advertise(pub_left);
     nh.advertise(pub_right);
     nh.subscribe(sub);
 }
 
 void loop () {
-    MotorTest(left_speed_percent / 100, right_speed_percent / 100, ena_pin, enb_pin);
+    MotorTest(left_speed_percent, right_speed_percent, ena_pin, enb_pin);
+
+    distance = sonar.ping_cm();
+    pub_dist.publish(&distance);
+
     nh.spinOnce();
 }
 
 
-// FUNCTIONS
+//Functions
 
 void motors_init(){
     // LEFT MOTOR
